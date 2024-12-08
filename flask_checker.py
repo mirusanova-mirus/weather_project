5338f8c003ad79189c42d3b6d5a5d0246fb6333d
mirus <mirusanova@gmail.com>
2024-12-08 16:24:50 +0100

    Подключение библиотеки flask

---
+++ flask_checker.py
@@ -0,0 +1,9 @@
+# Чекаем корректность работы flask
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return "Flask is working!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
